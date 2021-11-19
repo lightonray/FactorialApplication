64ab45082629e2a9daac49a87249fb73400f9a6f
American Computers <American Computers@LIGHTONRAY>
2021-11-19 21:25:54 +0200

    Add project files.

---
+++ FactorialApplication/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace FactorialApplication
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            {
+                try
+                {
+                    int number, factorial = 1; // declaring variables
+                    Console.WriteLine("Please enter a number: ");
+                    number = Convert.ToInt32(Console.ReadLine()); // converting to int32
+
+
+
+                    if (number < 0) // if condition
+
+                        Console.WriteLine("Can not find the factorial of a negative number");
+
+                    else if (number <= 1) // if "if" condition fails then else if runs
+
+                        Console.WriteLine("Factorial of number " + number + " is " + factorial);
+
+                    else
+                    {
+                        for (int counter = number; counter >= 2; counter--) //  for loop for the factorial of a number // n! = (n-1)..
+                        {
+                            factorial = factorial * counter;
+                        }
+
+                        Console.WriteLine("Factorial of number " + number + " is " + factorial);
+                    }
+                }
+
+                catch
+                {
+                    Console.WriteLine("Please enter a valid value");
+                }
+
+
+            }
+        }
+    }
+}
+    
+
